3d40e34e7124914b0f1dad87e06d3499bde02714
GrSveta <93871466+GrSveta@users.noreply.github.com>
2022-01-24 18:37:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ for (let i = 0; i < first.length; i++){
 first[i] = i + 1
 }
 
-function newFirst(){
-  
-for(let a = first.length -1; a > 0; a--){
-
-const b = Math.floor(Math.random() * (a - 1));
-
-[first[a],first[b]] = [first[b],first[a]];
-}
-view.innerHTML = first;
-}
+function newFirst(){  
+  for(let a = first.length -1; a > 0; a--){
+    const b = Math.floor(Math.random() * (a - 1));
+    [first[a],first[b]] = [first[b],first[a]];
+  }
+  view.innerHTML = first;
+  }
 newFirst();
 
 btn.addEventListener('click', newFirst);
